d86818fc92020632f336cf1f06e2b0cc7434a56c
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-19 04:40:16 +0100

    store update

--- store.js
+++ store.js
@@ -1,19 +1,18 @@
-export const useTodo = async (input) => {
+export const postTodo = async (input) => {
   try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos/1",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(input),
-      }
-    );
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    });
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
+    console.log(data);
+
     return data;
   } catch (error) {
     console.log(error);
