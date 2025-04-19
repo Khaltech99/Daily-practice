10e7a5ec8bb6af24fcc3aeffbef4b69b49bf2904
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-19 10:30:09 +0100

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Search from "./components/Search";
-import Home from "./components/Home";
-import Practice from "./components/Practice";
-import DashBoard from "./components/DashBoard";
-import Todo from "./components/Todo";
-import Movies from "./components/Movies";
+// import Search from "./components/Search";
+// import Home from "./components/Home";
+// import Practice from "./components/Practice";
+// import DashBoard from "./components/DashBoard";
+// import Todo from "./components/Todo";
+// import Movies from "./components/Movies";
 import PostPrac from "./components/PostPrac";
 
 const App = () => {
