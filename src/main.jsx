22f89327b37a43c7aec10d6f6ecf69cb0022bf94
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-21 22:15:52 +0100

    feat: implement routing for user display in main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router";
+import DisplayUser from "./components/displayUser.jsx";
 
 const todoClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={todoClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/users/:id" element={<DisplayUser />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 );
