23cbdd0ef1b0b4e2d706bb6e84d9190c6e5ed5c0
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-27 22:00:57 +0100

    feat: add convertRomanToIntegers function for Roman numeral conversion

--- leetcode.js
+++ leetcode.js
@@ -1,3 +1,5 @@
+import { LogIn } from "lucide-react";
+
 //PALINDROME CODE
 export const palindrome = (x) => {
   const array = x.toString();
@@ -7,3 +9,29 @@ export const palindrome = (x) => {
   }
   return false;
 };
+
+//CONVERT ROMAN FIGURE TO VALUES
+
+function convertRomanToIntegers(x) {
+  const input = x.toString().toUpperCase().split("");
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  return input.reduce((total, currentValue, currentIndex) => {
+    const currentNumber = romanNumerals[currentValue];
+    const nextNumber = romanNumerals[input[currentIndex + 1]];
+    if (currentNumber < nextNumber) {
+      return total - currentNumber;
+    } else {
+      return total + currentNumber;
+    }
+  }, 0);
+}
+
+console.log(convertRomanToIntegers("vi"));
