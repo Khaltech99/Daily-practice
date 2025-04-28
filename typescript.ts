0d806dbba1d20811ab8096be42215e39b8fa9ffa
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-28 23:31:30 +0100

    feat: add Book interface and example book object

---
+++ typescript.ts
@@ -0,0 +1,29 @@
+// interface Person {
+//   name: string;
+//   age: number;
+//   isMarried: boolean;
+//   hobbies: string[];
+// }
+// const lawal: Person = {
+//   name: "lawal",
+//   age: 23,
+//   isMarried: false,
+//   hobbies: ["reading", "coding", "swimming"],
+// };
+
+// function circumference(r: number): number {
+//   return 2 * Math.PI * r;
+// }
+
+interface Book {
+  title: string;
+  author: string;
+  isAvailabe: boolean;
+  pages: number;
+}
+const book: Book = {
+  title: "The Alchemist",
+  author: "Paulo Coelho",
+  isAvailabe: true,
+  pages: 208,
+};
