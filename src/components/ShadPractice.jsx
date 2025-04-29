54ac53d212bc863ef1c02b34fb223817f7d8f911
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-29 03:48:39 +0100

    feat: update ShadPractice component to include interactive elements and UI components

--- src/components/ShadPractice.jsx
+++ src/components/ShadPractice.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Button } from "./ui/button";
+import ProfileCard from "./ui/ProfileCard";
+import AlertUs from "./AlertUs";
+import BadgeUs from "./BadgeUs";
+import AlertDialogues from "./AlertDialogues";
 
 const ShadPractice = () => {
-  return <div>ShadPrac.tice</div>;
+  return (
+    <div>
+      <Button
+        className=" m-4 p-3 w-fit h-fit cursor-pointer"
+        variant="destructive"
+        size="sm"
+      >
+        click here
+      </Button>
+      <ProfileCard />
+      <AlertUs />
+      <BadgeUs />
+      <AlertDialogues />
+    </div>
+  );
 };
 
 export default ShadPractice;
