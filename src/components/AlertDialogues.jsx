443da9bb396497fadf00138d3cc7296e1e673528
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-29 03:48:09 +0100

    feat: add AlertDialogues component for user confirmation dialogs

---
+++ src/components/AlertDialogues.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+
+const AlertDialogues = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <AlertDialog>
+        <AlertDialogTrigger asChild className="border-destructive">
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default AlertDialogues;
