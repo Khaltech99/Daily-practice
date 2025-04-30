11e398afd72956d38a149c8b6a7895ef3b45fa31
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-04-30 02:55:25 +0100

    refactor: simplify ShadPractice component by removing unused imports and rendering only MyForm

--- src/components/ShadPractice.jsx
+++ src/components/ShadPractice.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-import { Button } from "./ui/button";
-import ProfileCard from "./ui/ProfileCard";
-import AlertUs from "./AlertUs";
-import BadgeUs from "./BadgeUs";
-import AlertDialogues from "./AlertDialogues";
+// import { Button } from "./ui/button";
+// import ProfileCard from "./ui/ProfileCard";
+// import AlertUs from "./AlertUs";
+// import BadgeUs from "./BadgeUs";
+// import AlertDialogues from "./AlertDialogues";
 
+import MyForm from "./MyForm";
 const ShadPractice = () => {
   return (
     <div>
-      <Button
-        className=" m-4 p-3 w-fit h-fit cursor-pointer"
-        variant="destructive"
-        size="sm"
-      >
-        click here
-      </Button>
-      <ProfileCard />
-      <AlertUs />
-      <BadgeUs />
-      <AlertDialogues />
+      <MyForm />
     </div>
   );
 };
