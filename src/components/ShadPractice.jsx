9e60db5da735413a1cfb7dce9dde06cb27ce53f0
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-05-08 20:33:57 +0100

    feat: add SheetPrac component to ShadPractice

--- src/components/ShadPractice.jsx
+++ src/components/ShadPractice.jsx
@@ -7,11 +7,13 @@ import React from "react";
 
 import MyForm from "./MyForm";
 import ComboBox from "./ComboBox";
+import SheetPrac from "./SheetPrac";
 const ShadPractice = () => {
   return (
     <div>
       <MyForm />
       <ComboBox />
+      <SheetPrac />
     </div>
   );
 };
