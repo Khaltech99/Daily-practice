15cbf74929eac2f7c9f2c24ae324f5e10914ceed
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-05-08 20:33:50 +0100

    feat: add initial TypeScript code with type definitions and functions for pets and vehicles

---
+++ practice.ts
@@ -0,0 +1,88 @@
+//tuples is assigning types to the each element of a known array considereing their respective positions
+
+// let mixture: [string, number, boolean] = ["", 32, false];
+
+// function getItems(name: string): boolean {
+//   return false;
+// }
+
+//funtion to sum
+
+// const sumUp = (numA: number, numB: string): number | string => {
+//   return numA + numB;
+// };
+
+// interface User {
+//   name: string;
+//   age: number;
+//   email: string;
+//   products: [];
+// }
+
+// const user = (input: User) => {
+//   return input.name;
+// };
+
+// type Us = number;
+// type Users = {
+//   name: string;
+//   age: number;
+// };
+
+// type User=Us|Users
+
+// function user(user: User) {
+//   console.log(user);
+// }
+
+// const write= (words:string):string|number=>{
+//   return words
+// }
+
+//Generics
+// function write<T>(words: T): T[] {
+//   return [words];
+// }
+
+// write(21);
+
+//LITERAL TYPES
+// type dropDown = "success" | "failure";
+
+// const dropdown: dropDown = "success";
+
+//ENUM
+// enum Roles {
+//   admin,
+//   vendor,
+//   buyers,
+// }
+
+// const admin: Roles = Roles.admin;
+
+// const user:Roles = "bola";
+
+type PetType = "dog" | "fish" | "cat";
+
+const petType: PetType = "fish";
+
+const getPetSound = (pet: PetType): string => {
+  if (pet === "dog") {
+    return "bark";
+  } else if (pet === "cat") {
+    return "meow";
+  }
+  return "sound of fish";
+};
+
+interface Vehicle {
+  name: string;
+}
+type Car = Vehicle & { doors: number };
+
+(result = [1, 1]), [2, 2];
+
+function repeat(value, times) {
+  let arr = [];
+  const result = arr.fill(value, times);
+}
