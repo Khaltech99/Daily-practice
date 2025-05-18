9aea878201b257e39a38358d3c348d6b4466d2f9
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-05-18 10:42:56 +0100

    feat: update ShadPractice component to use LoginCard and remove unused imports

--- src/components/ShadPractice.jsx
+++ src/components/ShadPractice.jsx
@@ -1,19 +1,18 @@
 import React from "react";
+import LoginCard from "./SignUp";
 // import { Button } from "./ui/button";
 // import ProfileCard from "./ui/ProfileCard";
 // import AlertUs from "./AlertUs";
 // import BadgeUs from "./BadgeUs";
 // import AlertDialogues from "./AlertDialogues";
 
-import MyForm from "./MyForm";
-import ComboBox from "./ComboBox";
-import SheetPrac from "./SheetPrac";
+// import MyForm from "./MyForm";
+// import ComboBox from "./ComboBox";
+// import SheetPrac from "./SheetPrac";
 const ShadPractice = () => {
   return (
     <div>
-      <MyForm />
-      <ComboBox />
-      <SheetPrac />
+      <LoginCard />
     </div>
   );
 };
