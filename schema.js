8def571531e706d244539a4fd5bc11f4b511bfad
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-05-18 10:41:54 +0100

    feat: add validation schemas for user registration, login, password reset, and update

---
+++ schema.js
@@ -0,0 +1,43 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  username: z.string().min(4, "name must be more than 4"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character"
+    ),
+  email: z.string().email("invalid email address"),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email("invalid email address"),
+  password: z.string(),
+});
+export const resetSchema = z.object({
+  email: z.string().email("invalid email address"),
+});
+
+export const updatePasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[^A-Za-z0-9]/,
+        "Password must contain at least one special character"
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
